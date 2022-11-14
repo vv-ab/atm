0e753c3f9686fc7256a09e5656cb9d66967caf46
Vivian Berger <vivianberger028@gmail.com>
2022-11-14 10:07:47 +0100

    Implemented workflows and configured gradle to run tests.

---
+++ src/test/scala/App.scala
@@ -0,0 +1,12 @@
+import atm
+
+import org.junit.runner.RunWith
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class App extends AnyFreeSpec {
+
+  ""
+  
+}
